2e5591eb4aa8eb19ea53d05840688f8c46ad2748
Mateusz Viste <mateusz@viste.fr>
2023-06-29 00:59:54 +0200

    upgraded SvarLANG to ver 20230629

--- SOURCE/FDISK/SVARLANG/svarlang.h
+++ SOURCE/FDISK/SVARLANG/svarlang.h
@@ -26,7 +26,7 @@
 #define SVARLANG_H
 
 /* library version */
-#define SVARLANGVER "20230628"
+#define SVARLANGVER "20230629"
 
 /* returns a pointer to a string with the SvarLANG's library version,
  * independently of the SVARLANGVER string above. */
