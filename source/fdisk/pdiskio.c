8126a214372bf0c55d6d9274d348ef61127b1c38
Bernd Boeckmann <bernd-freedos@boeckmann.io>
2024-05-11 19:00:36 +0200

    pdiskio.c: work around Xi8088 and Book8088 BIOS bug

--- source/fdisk/pdiskio.c
+++ source/fdisk/pdiskio.c
@@ -116,6 +116,9 @@ void Check_For_INT13_Extensions( void )
          r.h.ah = 0x41;
          r.w.bx = 0x55aa;
          r.h.dl = drive;
+         r.w.ds = 0x40; /* temporarly point to BDA to work around
+                           Xi8088 and Book8088 BIOS bug, see
+                           https://github.com/FDOS/kernel/issues/156 */
          intr( 0x13, &r );
 
          if ( ( ( r.w.flags & INTR_CF ) == 0 ) && ( r.w.bx == 0xaa55 ) ) {
